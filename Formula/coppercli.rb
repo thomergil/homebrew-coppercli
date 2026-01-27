9d45a20003e26225eeac3af8a2e5e58c88bb6b2c
Thomer Gil <thomer@thomer.com>
2026-01-26 19:46:56 -0500

    Fix checksums for v0.2.0

--- Formula/coppercli.rb
+++ Formula/coppercli.rb
@@ -7,16 +7,16 @@ class Coppercli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/thomergil/coppercli/releases/download/v0.2.0/coppercli-v0.2.0-osx-arm64.tar.gz"
-      sha256 "8f5fece3ee527ee312e92c0271250680a39cf184951d8ba57b4100249b128fd1"
+      sha256 "c79a3e85819c34142cb6d959ecea2ff76de7db827b250ba466ab37cb4fd60c4b"
     else
       url "https://github.com/thomergil/coppercli/releases/download/v0.2.0/coppercli-v0.2.0-osx-x64.tar.gz"
-      sha256 "33fc2c802492cebba3d4cbe35585d84a504fc7e038ab76ffccbf40c1567ab6cb"
+      sha256 "71f4fc23c7d0571900c72645ab78ff9b26a2ff732935ed289bd21b1ccb82d61f"
     end
   end
 
   on_linux do
     url "https://github.com/thomergil/coppercli/releases/download/v0.2.0/coppercli-v0.2.0-linux-x64.tar.gz"
-    sha256 "20d3fdcd00e46e2dd103945f9dbd041db784b402eb283c9f8ad4b3bd0e433b08"
+    sha256 "a9bf224333378c4c06303596d44f291e0c9d8cc5ebac14eb7f7202d6442f2b62"
   end
 
   def install
